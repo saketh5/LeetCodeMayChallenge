20e3acb0d267313af45f7b8aaa3683b7e5753649
saketh5 <msaketh92@gmail.com>
2020-05-07 01:09:00 +0530

    Add files via upload

---
+++ firstUniqueChar.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        
+        int[] num = new int[128];
+        for( int i = 0; i < s.length(); i++) {
+            num[s.charAt(i)-'a']++;
+        }
+        for( int i = 0; i <s.length(); i++) {
+            if( num[s.charAt(i)-'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
